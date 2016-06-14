e3a7d7dbdc7f061c90dbfecfe89342bb41869f47
cthanhnguyen <cthanhnguyen@github.com>
2016-06-14 17:48:21 +0700

    revise banner

--- client/app/components/banner/banner.controller.js
+++ client/app/components/banner/banner.controller.js
@@ -4,30 +4,8 @@ export default class BannerCtrl {
   constructor() {
     this.setup();
   }
-
   setup() {
-    
-  }
-  
-  prebook() {
-    this.show3 = false;
-    this.show31 = true;
-    this.preview = true;
-    this.step3 = false;
-  };
-
-  editbook() {
-    this.show3 = true;
-    this.show31 = false;
-    this.preview = false;
-    this.step3 = true;
-    this.next31 = true;
-  };
-
-  placebook() {
-    this.finalstep = false;
-    this.campaign = false;
-    this.step3_2 = true;
-    this.actionprocess = "Processing...";
+    this.banner = true;
+    CKEDITOR.replace( 'htmlEditor' ); 
   }
 }
