a34c95a4178504ab20f9dfa8faf39c76df9adf79
cthanhnguyen <cthanhnguyen@github.com>
2016-06-21 16:47:24 +0700

    fix gulp

--- common/jobs/observer-campaign-completed.js
+++ common/jobs/observer-campaign-completed.js
@@ -5,7 +5,8 @@ export default (app, agenda)=> {
   const {ReportTracking1, Campaign} = models.Campaign;
   agenda.define("observer-campaign-completed", {concurrency: 1, priority: 'normal'}, (job, cb)=> {
     // find all to list hit database 2 time
-    let allActiveCampain = Campaign.find({where:{active:!null}})
-
+    let allActiveCampain = Campaign.find({where: {active: {"neq": null}}})
+    let campaignIdArray = allActiveCampain.map((item)=>(item.id))
+    let allRefernceReportClick = ReportTracking1.find({where:{campaignId:{"inq":campaignIdArray}}})
   })
 }
