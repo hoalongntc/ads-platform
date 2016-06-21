ebcdf6d9923962ce48f3449f385dbfa2783c0d04
TriLe <thanhtrixx@gmail.com>
2016-06-21 09:39:19 +0700

    Save  campaign

--- client/app/components/campaign/campaign.controller.js
+++ client/app/components/campaign/campaign.controller.js
@@ -3,12 +3,13 @@ import moment from 'moment'
 require('angucomplete-alt');
 
 export default class CampaignCtrl {
-  constructor(Campaign, CommonData, Location, Brand, Banner) {
+  constructor(Campaign, CommonData, Location, Brand, Banner, CampaignMapping) {
     this.Campaign = Campaign;
     this.CommonData = CommonData;
     this.Location = Location;
     this.Banner = Banner;
     this.Brand = Brand;
+    this.CampaignMapping = CampaignMapping;
     this.setup();
   }
 
@@ -167,7 +168,8 @@ export default class CampaignCtrl {
           kpiPerDay: location.kpi
         };
       });
-    if(this.selected.locations.length < 0 || !this.selected.adSet)
+
+    if(this.selected.locations.length <= 0 || !this.selected.adSet)
       return;
 
     $('#adset-icon').css('color', '#45E252');
@@ -225,6 +227,11 @@ export default class CampaignCtrl {
     let scheduleToStr = this.selected.scheduleTo.format('YYYYMMDD') + this.selected.timeTo.id;
     let scheduleTo = moment(scheduleToStr, 'YYYYMMDDH').valueOf();
 
+    let audience = {
+      age: [ this.selected.ageFrom.id, this.selected.ageTo.id ],
+      gender: this.selected.gender.value,
+      os: this.selected.device.value
+      };
 
     this.campaignObj = {
       active: false,
@@ -233,18 +240,16 @@ export default class CampaignCtrl {
       brandName: this.selected.brand.name,
       categoryId: this.selected.campaignCategory.value,
       categoryName: this.selected.campaignCategory.text,
-      audience: {
-        age: [ this.selected.ageFrom.id, this.selected.ageTo.id ],
-        gender: this.selected.gender.value,
-        os: this.selected.device.value
-      },
+      audience: audience,
       scheduleFrom: scheduleFrom,
       scheduleTo: scheduleTo,
       kpi: this.selected.kpi,
       name: this.selected.adSet,
       landingPageUrl: this.selected.landingPage,
-      bannerDesktop: this.selected.bannerDesktop,
-      bannerMobile: this.selected.bannerMobile,
+      bannerDesktopId: this.selected.bannerDesktop.originalObject.id,
+      bannerDesktopName: this.selected.bannerDesktop.originalObject.name,
+      bannerMobileId: this.selected.bannerMobile.originalObject.id,
+      bannerMobileName: this.selected.bannerMobile.originalObject.name,
       locations: this.selected.locations
     };
 
@@ -252,11 +257,34 @@ export default class CampaignCtrl {
       .$promise
       .then((response) => {
         this.campaignObj = response;
-        this.actionSuccess = true
       })
-      .catch((response) => {
-        console.error(response.data.error);
+      .then((response) => {
+        let priority = new Date().getTime() / 1000;
+        let compaignMappings = this.locations
+          .filter((location) => { return location.selected === true; })
+          .map((location) => {
+            return {
+              campaignId: this.campaignObj.id,
+              campaignName: this.campaignObj.name,
+              audience: audience,
+              locationId: location.id,
+              locationName: location.name,
+              kpiPerDay: location.kpi,
+              priority: priority
+            };
+          });
+        this.CampaignMapping.createMany(compaignMappings)
+          .$promise
+          .then((response) => {
+            this.actionprocess = "Your booking has been placed!";
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       })
+      .catch((error) => {
+        console.error(error);
+      });
 
     this.actionprocess = "Processing...";
   }
@@ -267,5 +295,5 @@ export default class CampaignCtrl {
 }
 
 export default angular
-  .module('campaign.controller', [])
+  .module('campaign.controller', ['angucomplete-alt'])
   .controller('CampaignCtrl', CampaignCtrl);
